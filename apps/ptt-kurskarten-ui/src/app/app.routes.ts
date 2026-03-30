df67d648f06f72f38bf56af81789570312613752
Suja K <message@sujathan.ch>
2026-03-30 14:08:44 +0200

    refactor paths

--- apps/ptt-kurskarten-ui/src/app/app.routes.ts
+++ apps/ptt-kurskarten-ui/src/app/app.routes.ts
@@ -1,28 +1,32 @@
 import { Routes } from '@angular/router';
-import { ADMIN_GRAPH_REPOSITORY, DemoGraphRepository, HttpGraphRepository } from './features/admin/admin-graph.repository';
-import { environment } from '../environments/environment';
+import {
+  ADMIN_GRAPH_REPOSITORY,
+  DemoGraphRepository,
+  HttpGraphRepository
+} from '@admin/admin-graph.repository';
+import { environment } from '@env/environment';
 
 const fullRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/viewer/viewer.component').then((m) => m.ViewerComponent)
+    loadComponent: () => import('@viewer/viewer.component').then((m) => m.ViewerComponent)
   },
   {
     path: 'connections',
-    loadComponent: () => import('./features/connections/connections.component').then((m) => m.ConnectionsComponent)
+    loadComponent: () => import('@connections/connections.component').then((m) => m.ConnectionsComponent)
   },
   {
     path: 'reports',
-    loadComponent: () => import('./features/reports/reports.component').then((m) => m.ReportsComponent)
+    loadComponent: () => import('@reports/reports.component').then((m) => m.ReportsComponent)
   },
   {
     path: 'admin/tutorial',
-    loadComponent: () => import('./features/admin/admin.component').then((m) => m.AdminComponent),
+    loadComponent: () => import('@admin/admin.component').then((m) => m.AdminComponent),
     providers: [{ provide: ADMIN_GRAPH_REPOSITORY, useClass: DemoGraphRepository }]
   },
   {
     path: 'admin',
-    loadComponent: () => import('./features/admin/admin.component').then((m) => m.AdminComponent),
+    loadComponent: () => import('@admin/admin.component').then((m) => m.AdminComponent),
     providers: [{ provide: ADMIN_GRAPH_REPOSITORY, useClass: HttpGraphRepository }]
   }
 ];
@@ -30,7 +34,7 @@ const fullRoutes: Routes = [
 const readonlyRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./features/viewer/viewer.component').then((m) => m.ViewerComponent)
+    loadComponent: () => import('@viewer/viewer.component').then((m) => m.ViewerComponent)
   }
 ];
 
